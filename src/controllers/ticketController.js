76ecd4e5d72da81578ebcaaea1c6cc7f80a67368
Grecia Delgado <greciadm2596@gmail.com>
2025-02-28 17:52:31 -0500

    Update schemas and controllers

--- src/controllers/ticketController.js
+++ src/controllers/ticketController.js
@@ -3,48 +3,47 @@ import Movie from "../models/Movie.js";
 
 // Crear un ticket
 export const createTicket = async (req, res) => {
-	try {
-		const { userName, userEmail, movieId, date, time, tickets, seats } = req.body;
+  try {
+    const { userName, userEmail, movieId, date, time, tickets, seats } =
+      req.body;
 
-		console.log("movie ID", movieId)
+    console.log("movie ID", movieId);
 
-		const movie = await Movie.findById(movieId);
-		if (!movie)
-			return res.status(404).json({ message: "Película no encontrada" });
+    const movie = await Movie.findById(movieId);
+    if (!movie)
+      return res.status(404).json({ message: "Película no encontrada" });
 
-		console.log("PELICULA ENCONTRADA", movie);
-		
-		const totalPrice = movie.price * tickets;
+    console.log("PELICULA ENCONTRADA", movie);
 
-		const newTicket = new Ticket({
-			userName,
-			userEmail,
-			movieTitle: movie.title,
-			date,
-			time,
-			tickets,
-			price: movie.price,
-			totalPrice,
-			seats,
-		});
+    const totalPrice = movie.price * tickets;
 
-		await newTicket.save();
-		res
-			.status(201)
-			.json({ message: "Compra realizada con éxito", ticket: newTicket });
-	} catch (error) {
-		res.status(500).json({ message: "Error al procesar la compra" });
-	}
+    const newTicket = new Ticket({
+      userName,
+      userEmail,
+      movieTitle: movie.title,
+      date,
+      time,
+      tickets,
+      price: movie.price,
+      totalPrice,
+      seats,
+    });
+
+    await newTicket.save();
+    res.status(201).json({ ticketId: newTicket.id });
+  } catch (error) {
+    res.status(500).json({ message: "Error al procesar la compra" });
+  }
 };
 
 // Obtener un ticket por ID
 export const getTicketById = async (req, res) => {
-	try {
-		const ticket = await Ticket.findById(req.params.id);
-		if (!ticket)
-			return res.status(404).json({ message: "Ticket no encontrado" });
-		res.json(ticket);
-	} catch (error) {
-		res.status(500).json({ message: "Error al obtener el ticket" });
-	}
+  try {
+    const ticket = await Ticket.findById(req.params.id);
+    if (!ticket)
+      return res.status(404).json({ message: "Ticket no encontrado" });
+    res.json(ticket);
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener el ticket" });
+  }
 };
